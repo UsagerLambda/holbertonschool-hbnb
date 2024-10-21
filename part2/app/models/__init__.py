42e9c79256d21757c94ae09bb1b5d598a2f50164
Utilisateur Lambda <clement.chassem@gmail.com>
2024-10-21 15:09:30 +0200

    Add T2

--- part2/app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .amenities import Amenity
-from .places import Place
-from .reviews import Review
-from .users import User
--- part2/app/models/users.py
+++ part2/app/models/users.py
@@ -1,4 +1,5 @@
-from baseModel import BaseModel
+from app.models.baseModel import BaseModel
+
 
 class User(BaseModel):
     def __init__(self, first_name, last_name, email, is_admin=False):
