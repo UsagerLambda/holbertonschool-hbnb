3bd7ddb0426f39f3ba26fa771ca1032ff9e9a5db
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-10-24 15:06:47 +0200

    Testing

--- part2/app/models/reviews.py
+++ part2/app/models/reviews.py
@@ -1,7 +1,7 @@
 from app.models.baseModel import BaseModel
 
 class Review(BaseModel):
-    def __init__(self, text, rating, place, user, user_id):
+    def __init__(self, text, rating, place, place_id, user, user_id):
         super().__init__()
         if not (1 <= rating <= 5):
             raise ValueError("Rating must be between 1 and 5")
@@ -10,6 +10,7 @@ class Review(BaseModel):
         self.user_id = user_id
         self.rating = rating
         self.place = place
+        self.place = place_id
         self.user = user
 
 def to_dict(self):
