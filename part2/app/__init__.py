099df78092853b43599f7cdc6624abba03af5469
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-10-24 09:57:33 +0200

    Deleted Pycache files and updated run.py file

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -3,8 +3,8 @@ from flask_restx import Api
 
 from app.api.v1.users import api as users_ns
 from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.places import api as places_ns
 from app.api.v1.amenities import api as amenities_ns
-from app.api.v1.places import api as places_ns  #added
 
 
 def create_app():
