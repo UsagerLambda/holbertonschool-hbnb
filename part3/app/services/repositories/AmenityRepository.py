68f3dcb1906800f93180a19416b7e2f8fbd334a2
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-11-17 15:58:43 +0100

    Edited AmenityRepository import

--- part3/app/services/repositories/AmenityRepository.py
+++ part3/app/services/repositories/AmenityRepository.py
@@ -2,6 +2,6 @@ from app.models.amenities import Amenity
 from app import db
 from app.persistence.repository import SQLAlchemyRepository
 
-class ReviewRepository(SQLAlchemyRepository):
+class AmenityRepository(SQLAlchemyRepository):
     def __init__(self):
         super().__init__(Amenity)
