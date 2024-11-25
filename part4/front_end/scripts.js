941460150a62ff518882e3b8b5808e98dbfe432c
Utilisateur Lambda <clement.chassem@gmail.com>
2024-11-25 09:43:12 +0100

    Add Filter Function for index.html

--- part4/front_end/scripts.js
+++ part4/front_end/scripts.js
@@ -1,9 +1,42 @@
-// URL LOGIN
+// ROUTE API ================================================================================== //
+
 const API_URL = 'http://127.0.0.1:5000/api/v1/auth/login';
+const GET_ALL_PLACES = 'http://127.0.0.1:5000/api/v1/places';
+const GET_PLACE = 'http://127.0.0.1:5000/api/v1/places/{place_id}'
+const POST_REVIEW = 'http://127.0.0.1:5000/api/v1/reviews';
+const GET_REVIEWS_FROM_PLACE = 'http://127.0.0.1:5000/api/v1/reviews/places/{place_id}';
+
+// URL HTML =================================================================================== //
+
 const LOGIN_PAGE_URL = 'http://127.0.0.1:5500/part4/front_end/login.html';
 const INDEX_PAGE_URL = 'http://127.0.0.1:5500/part4/front_end/index.html';
 const PLACE_PAGE_URL = 'http://127.0.0.1:5500/part4/front_end/place.html';
 const REVIEW_PAGE_URL = 'http://127.0.0.1:5500/part4/front_end/add_review.html';
+
+// AUTH ======================================================================================= //
+
+// Fonction qui vérifie si l'utilisateur est loggé ou non
+function checkAuthentication() {
+  const token = getCookie('token');
+  const loginLink = document.getElementById('login-link');
+  if (!token) {
+    loginLink.style.display = 'block';
+  } else {
+    loginLink.style.display = 'none';
+}
+}
+// Fonction pour récupérer le cookie et ainsi vérifier si l'utilisateur est loggé
+function getCookie(name) {
+  const cookies = document.cookie.split(";");
+  for (cookie of cookies) {
+    if (cookie.startsWith(name + "=")) {
+      return cookie.substring(name.length + 1);
+    }
+  }
+  return null;
+}
+
+// LOGIN ====================================================================================== //
 
 // Fonction pour gérer les cookies
 function setAuthCookie(token) {
@@ -13,14 +46,14 @@ function setAuthCookie(token) {
     document.cookie = `token=${token}; expires=${expiryDate.toUTCString()}; path=/; SameSite=Strict`;
 }
 
-function loginRequest() {
+function loginLoad() {
     // Récupère le formulaire de login par son ID CSS
     const loginForm = document.getElementById('login-form');
 
     // si l'id existe dans la page
     if (loginForm) {
         loginForm.addEventListener('submit', async (event) => {
-            event.preventDefault();
+            event.preventDefault(); // empêche le rechargement par défaut de la page après avoir submit les infos du login
 
             try {
                 // Récupère les données du formulaire
@@ -63,55 +96,109 @@ function loginRequest() {
     }
 }
 
+// INDEX ====================================================================================== //
+
+// inutile mais harmonise le nom des fonctions de base dans le DOMContentLoader
 function indexLoad() {
-  checkAuthentication();
-  loadPlaceIndex();
+  fetchPlaces();
 }
 
-function loadPlaceIndex() {
-  console.log("loaded");
-}
+// PLACES INDEX =============================================================================== //
 
-function checkAuthentication() {
-  const token = getCookie('token');
-  const loginLink = document.getElementById('login-link');
-  if (!token) {
-    loginLink.style.display = 'block';
-  } else {
-    loginLink.style.display = 'none';
-    // Fetch places data if the user is authenticated
-    fetchPlaces(token);
-}
-}
-function getCookie(name) {
-  console.log(name);
-  const cookies = document.cookie.split(";");
-  for (cookie of cookies) {
-    if (cookie.startsWith(name + "=")) {
-      return cookie.substring(name.length + 1);
+// fonction pour récupérer la liste de places dans la DB
+async function fetchPlaces() {
+  try {
+    // Effectue une requête API pour récupérer tous les lieux
+    const response = await fetch(GET_ALL_PLACES);
+
+    if (!response.ok) {
+      throw new Error(`Erreur HTTP : ${response.status}`);
     }
+
+    // Convertit le contenu de la réponse au format JSON et appelle une fonction pour l'afficher
+    const places = await response.json();
+    displayPlaces(places);
+    return places;
+
+  } catch (error) {
+    console.error('Erreur de chargement des places :', error);
   }
-  return null;
 }
 
+function displayPlaces(places) {
+  // récupère l'id "place-list" de la balise section de index.html
+  const placesList = document.getElementById('places-list');
+  // vide le contenu existant dans la section
+  placesList.innerHTML = '';
+  // boucle dans le dictionnaire "places", crée un élément div pour chaque lieu ajoute des class CSS pour le style
+  // crée un attribut personnalisé pour le prix du lieu (pour le trier plus tard) et l'id de la place puis rempli le contenue HTML avec les infos du lieu
+  places.forEach(place => {
+    const placeElement = document.createElement('div');
+    placeElement.classList.add('card', 'place-card');
+    placeElement.dataset.price = place.price;
+    placeElement.dataset.placeId = place.id;
+    placeElement.innerHTML = `
+      <h3 class="place-name">${place.title}</h3>
+      <p class="place-price">Price per night: $${place.price}</p>
+      <a href="place.html" class="button login-button">View Details</a>`;
+    // Ajoute l'élément
+    placesList.appendChild(placeElement);
+  });
+  priceFilter();
+}
 
-// Vérification de l'URL avant d'appeler loginRequest
+// PRICES FILTERING INDEX ===================================================================== //
+
+function priceFilter() {
+  const priceSelect = document.getElementById('price-filter'); // récup filtre
+  const placeCards = document.querySelectorAll('.place-card'); // récup les cartes de lieux
+
+  // regarde si le filtre change si oui convertie la valeur en int
+  priceSelect.addEventListener('change', (event) => {
+    const selectedPrice = parseInt(event.target.value); // Convertir en nombre
+
+    placeCards.forEach(card => { // Parcourt toutes les cartes
+      const placePrice = parseInt(card.dataset.price); // Récup prix stocké dans la carte du lieu
+
+      // si le prix de la place est inférieur ou égal au prix selectionné (0 si 'All' est select)
+      if (selectedPrice === 0 || placePrice <= selectedPrice) {
+        card.style.display = 'block'; // affiche la carte
+      } else { // sinon
+        card.style.display = 'none'; // cache la carte
+      }
+    });
+  });
+}
+
+// PLACE ====================================================================================== //
+
+function placeLoad() {
+  console.log("place")
+}
+
+// REVIEW ===================================================================================== //
+
+function reviewLoad() {
+  console.log("review")
+}
+
+// LOADER ===================================================================================== //
+
 document.addEventListener('DOMContentLoaded', () => {
+  checkAuthentication();
     if (window.location.href === LOGIN_PAGE_URL) {
-      console.log("LOGIN")
-      loginRequest();
+      loginLoad();
   }
 
     if (window.location.href === INDEX_PAGE_URL) {
-      console.log("INDEX")
       indexLoad();
   }
 
     if (window.location.href === PLACE_PAGE_URL) {
-      console.log("PLACE")
+      placeLoad();
   }
 
     if (window.location.href === REVIEW_PAGE_URL) {
-      console.log("REVIEW")
+      reviewLoad();
   }
 });
